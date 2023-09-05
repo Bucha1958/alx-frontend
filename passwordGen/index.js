daaf23d0be7401bbc04be681db2e80e6ce38a877
Bucha1958 <okoriemmadu@gmail.com>
2023-09-05 05:44:37 -0700

    passwords

--- passwordGen/index.js
+++ passwordGen/index.js
@@ -1,11 +1,14 @@
 
-const characters =[
+const characters = [
     "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V",
     "W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
     "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")",
     "_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
 ];
 
+let inputParam = document.getElementById('input-parameter');
+
+let inputParam2 = document.getElementById('input-parameter2');
 
 function getRandomChar() {
     let random = Math.floor(Math.random() * characters.length);
@@ -15,10 +18,12 @@ function getRandomChar() {
 
 function getRandomPass() {
     let random = "";
+    let random2 = "";
     for (let i = 0; i < 15; i++) {
         random += getRandomChar();
-    }
-    console.log(random);
-}
+        random2 += getRandomChar();
 
-getRandomPass();
+    }
+    inputParam.textContent = random;
+    inputParam2.textContent = random2;
+}
